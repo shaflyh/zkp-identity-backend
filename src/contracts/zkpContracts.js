a6f9892e6d0a1ab05821996c92fa4143913ec01c
Shafly Hamzah <shafly2000@gmail.com>
2025-04-25 13:05:02 +0700

    feat: add polygon mainnet config

--- src/contracts/zkpContracts.js
+++ src/contracts/zkpContracts.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const contract = require("./IdentityZKP.json");
 const abi = contract.abi;
-const provider = new ethers.JsonRpcProvider(process.env.POLYGON_AMOY_RPC_URL);
+const provider = new ethers.JsonRpcProvider(process.env.POLYGON_MAINNET_RPC_URL);
 const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
 const identityContract = new ethers.Contract(process.env.IDENTITY_CONTRACT_ADDRESS, abi, signer);
